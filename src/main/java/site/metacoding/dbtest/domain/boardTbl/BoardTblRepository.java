548089783142f924a4806ec501112510877d7896
a01048197443 <a01048197443@gmail.com>
2022-03-24 12:38:04 +0900

    6. ajax로 csr하기 - with jquery

--- src/main/java/site/metacoding/dbtest/domain/boardTbl/BoardTblRepository.java
+++ src/main/java/site/metacoding/dbtest/domain/boardTbl/BoardTblRepository.java
@@ -1,7 +1,11 @@
 package site.metacoding.dbtest.domain.boardTbl;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardTblRepository extends JpaRepository<BoardTbl, Integer> {
-
+    @Query(value = "SELECT * FROM boardTbl WHERE title like %:keyword%", nativeQuery = true)
+    List<BoardTbl> mSearch(String keyword);
 }
